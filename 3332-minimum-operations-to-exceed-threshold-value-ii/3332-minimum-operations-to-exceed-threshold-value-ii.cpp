63338e6a78cdfcc749911a845a185969d5a665d5
Saurabh Kumar <76895770+Saurabh29-hub@users.noreply.github.com>
2025-02-13 12:55:05 +0530

    Time: 102 ms (34.22%), Space: 99.1 MB (39.26%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,21 +1,33 @@
 class Solution {
- public:
-  int minOperations(vector<int>& nums, int k) {
-    int ans = 0;
-    priority_queue<long, vector<long>, greater<>> minHeap;
-
-    for (const int num : nums)
-      minHeap.push(num);
-
-    while (minHeap.size() > 1 && minHeap.top() < k) {
-      const int x = minHeap.top();
-      minHeap.pop();
-      const int y = minHeap.top();
-      minHeap.pop();
-      minHeap.push(min(x, y) * 2L + max(x, y));
-      ++ans;
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int operations = 0;
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        
+        // Add all elements to min heap
+        for(int num : nums) {
+            pq.push((long long)num);
+        }
+        
+        // Continue operations until all elements are >= k
+        while(pq.size() >= 2 && pq.top() < k) {
+            // Get two smallest elements
+            long long x = pq.top(); pq.pop();
+            long long y = pq.top(); pq.pop();
+            
+            // Calculate new value
+            long long newVal = min(x, y) * 2 + max(x, y);
+            
+            // Add new value back to heap
+            pq.push(newVal);
+            operations++;
+        }
+        
+        // Check if smallest element is still less than k
+        if(pq.top() < k) {
+            return -1;
+        }
+        
+        return operations;
     }
-
-    return ans;
-  }
 };
